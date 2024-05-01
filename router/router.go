d146a61385c69451655d286826b167d06232b1f4
William <wmoura85@gmail.com>
2024-05-01 00:53:59 -0300

    feat: testando primeiro commit

--- router/router.go
+++ router/router.go
@@ -9,7 +9,7 @@ func Initialize(){
 	router := gin.Default()
 	router.GET("/ping", func(c *gin.Context) {
 	  c.JSON(http.StatusOK, gin.H{
-		"message": "pong pdfdf",
+		"message": "pong",
 	  })
 	})
 	router.Run(":8080") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
